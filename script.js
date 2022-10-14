4898569393f8018d4f59efc986a4efd21e8c884d
adyanshkakran <adyanshkakran@gmail.com>
2022-10-14 15:28:52 +0530

    Added part of KeyError

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
-function Test() {
-    var x = document.getElementById("num").value;
-    if (!(x>=0)) {
-        document.getElementById("demo").innerHTML = "ValueError: math domain error";
-        animate(1);
-    }
-    else {
-        document.getElementById("demo").innerHTML = "Square Root of " + x + " is "+ Math.sqrt(x);
-        animate(-1);
-        event.currentTarget.value = "";
-    }
-
-}
+let experiments = ['ValueError','KeyError','IndexError','ZeroDivisionError']
 function animate(i) {
     if(i==1){
-        document.getElementById("reward").innerHTML = "You are an exception master!";
     } else {
-        document.getElementById("reward").innerHTML = "You need a little more rebelion. Try again!";
-        document.getElementById("refresh").style.display='block';
     }
 }
 function handleSubmit(){
@@ -30,7 +15,55 @@ function handleSubmit(){
         answer.innerHTML = "Correct! ValueError Exception will be raised\n whenever user enters anything but a number"
     else
         answer.innerHTML = "Incorrect! Try Again"
-}
+    }
 function Refresh() {
     location.reload();
+}
+function changeExperiment() {
+    let experiment = document.getElementById("exception-name").value
+    experiments.forEach(exp => {
+        let elements = document.getElementsByClassName(exp)
+        if(exp === experiment){
+            elements[0].style.display = 'block'
+            elements[1].style.display = 'block'
+        }else{
+            elements[0].style.display = 'none'
+            elements[1].style.display = 'none'
+        }
+    });
+    
+}
+function TestValueError() {
+    var x = document.getElementById("num").value;
+    if (!(x>=0)) {
+        document.getElementById("demo").innerHTML = "ValueError: math domain error";
+        document.getElementById("reward").innerHTML = "You are an exception master!";
+    }
+    else {
+        document.getElementById("demo").innerHTML = "Square Root of " + x + " is "+ Math.sqrt(x);
+        document.getElementById("reward").innerHTML = "You need a little more rebelion. Try again!";
+        document.getElementById("refresh").style.display='block';
+        event.currentTarget.value = "";
+    }
+
+}
+function TestKeyError(){
+    let name = document.getElementById("name").value
+    let age = -1
+    if(name == 'John')
+        age = 30
+    else if(name == 'Adam')
+        age = 28
+    else if(name == 'Jack')
+        age = 37
+    console.log(age)
+    if(age == -1){
+        document.getElementById("demoKey").innerHTML = "KeyError: '" + name+"'";
+        document.getElementById("rewardKey").innerHTML = "You are an exception master!";
+    }else{
+        document.getElementById("demoKey").innerHTML = "Age of " + name + " is " + age;
+        document.getElementById("rewardKey").innerHTML = "You need a little more rebelion. Try again!";
+        document.getElementById("refreshKey").style.display='block';
+        event.currentTarget.value = "";
+    }
 }
